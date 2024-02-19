5ba6602ca173c07cce9f50fc0e24e2c1fdd45998
weverton <weverton.wrb10@gmail.com>
2024-02-19 11:35:47 -0300

    Final update
    Final update

--- timer/assets/js/main.js
+++ timer/assets/js/main.js
@@ -1,31 +1,46 @@
-const clock = document.querySelector('.clock');
-const start = document.querySelector('.start');
-const pause = document.querySelector('.pause');
-const reset = document.querySelector('.reset');
-let seconds = 0;
-
-function getTimeFromSeconds(seconds){
-    const data = new Date(seconds * 1000);
-    return data.toLocaleTimeString('pt-BR', 
-    {hour12: false, timeZone: 'GMT' // or 'UTC'
-})
+function clock () {
+    const clock = document.querySelector('.clock');
+    const start = document.querySelector('.start');
+    const pause = document.querySelector('.pause');
+    const reset = document.querySelector('.reset');
+    let seconds = 0;
+    let timer;
+    
+    function getTimeFromSeconds(seconds){
+        const data = new Date(seconds * 1000);
+        return data.toLocaleTimeString('pt-BR', 
+        {hour12: false, timeZone: 'GMT' // or 'UTC'
+    })
+    }
+    
+    function startClock() {
+        timer = setInterval(function() {
+            seconds++;
+            clock.innerHTML = getTimeFromSeconds(seconds);
+        }, 1000);
+    }
+    
+    document.addEventListener('click', function(e) {
+        const element = e.target;
+        
+        if (element.classList.contains('reset')) {
+            clearInterval(timer);
+            clock.classList.remove('pauseStyle');
+            clock.innerHTML = '00:00:00';
+            seconds = 0;
+        }
+    
+        if (element.classList.contains('pause')) {
+            clock.classList.add('pauseStyle');
+            clearInterval(timer);
+        }
+    
+        if (element.classList.contains('start')) {
+            clock.classList.remove('pauseStyle');
+            clearInterval(timer);
+            startClock();
+        }
+    
+    });
 }
-
-function startClock() {
-    const timer = setInterval(function() {
-        seconds++;
-        clock.innerHTML = getTimeFromSeconds(seconds);
-    }, 1000)
-}
-
-start.addEventListener('click', function(event){
-    startClock()
-});
-
-pause.addEventListener('click', function(event){
-    alert('Pause button initiated');
-});
-
-reset.addEventListener('click', function(event){
-    alert('Reset button initiated');
-});
+clock();
